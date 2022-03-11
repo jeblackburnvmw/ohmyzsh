a2382fcc71227ae42caab4f46961f8ffeb5a20c8
jeblackburnvmw <96553928+jeblackburnvmw@users.noreply.github.com>
2022-03-10 17:28:45 -0700

    Add an alias to apply a Kustomize overlay
    https://kubectl.docs.kubernetes.io/references/kubectl/apply/#apply-with-kustomize-files

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -28,6 +28,9 @@ alias kca='_kca(){ kubectl "$@" --all-namespaces;  unset -f _kca; }; _kca'
 # Apply a YML file
 alias kaf='kubectl apply -f'
 
+# Apply a Kustomize overlay
+alias kak='kubectl apply -k'
+
 # Drop into an interactive terminal on a container
 alias keti='kubectl exec -t -i'
 
